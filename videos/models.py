ced8516cd99be754e210f80719fb01ff74b771c8
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-28 20:09:09 -0300

    add: first model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    video_id = models.CharField(max_length=100)
