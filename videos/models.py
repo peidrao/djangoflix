4cad11aa3ab72f1681fbb821f1251dfe18ff18bc
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-28 20:37:57 -0300

    add: video proxy

--- videos/models.py
+++ videos/models.py
@@ -6,3 +6,13 @@ class Video(models.Model):
     description = models.TextField()
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f'id: {self.id} - title: {self.title}'
+
+
+class VideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
