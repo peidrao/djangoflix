c5bb12671e19a5096a97f731c98af118a30d32cc
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-28 20:56:18 -0300

    update: model and admin

--- videos/admin.py
+++ videos/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 
-from .models import Video, VideoProxy
+from .models import Video, VideoPublishedProxy, VideoAllProxy
 
 
-admin.site.register(Video)
-admin.site.register(VideoProxy)
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id', 'is_published']
+    search_fields = ['title']
+    list_filter = ['is_active']
+    readonly_fields = ['id', 'is_published', 'timestamp']
+
+    class Meta:
+        model = VideoAllProxy
+
+
+class VideoProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    search_fields = ['title']
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(is_active=True)
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+admin.site.register(VideoPublishedProxy, VideoProxyAdmin)
--- videos/models.py
+++ videos/models.py
@@ -6,13 +6,30 @@ class Video(models.Model):
     description = models.TextField()
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return f'id: {self.id} - title: {self.title}'
 
+    @property
+    def timestamp(self):
+        return None
 
-class VideoProxy(Video):
+    @property
+    def is_published(self):
+        return self.is_active
+
+
+class VideoPublishedProxy(Video):
     class Meta:
         proxy = True
         verbose_name = 'Published Video'
         verbose_name_plural = 'Published Videos'
+
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
