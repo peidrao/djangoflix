30af2149bd74944f51463dbf0e1654d1d0618b5a
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-29 10:30:15 -0300

    add: signals

--- videos/models.py
+++ videos/models.py
@@ -1,13 +1,22 @@
-from enum import unique
+
 from django.db import models
+from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.text import slugify
+
+
+class PublishedStateOptions(models.TextChoices):
+    # CONSTANT = DB_VALUE, USER_DISPLAY_VA
+    PUBLISHED = 'PU', 'Publish'
+    DRAFT = 'DR', 'Draft'
+    # UNLISTED = 'UN', 'Publish'
+    # PRIVATE = 'PR', 'Private'
 
 
 class VideoQuerySet(models.QuerySet):
     def published(self):
         now = timezone.now()
-        return self.filter(state=Video.VideoStateOptions.PUBLISHED, published_timestamp__lte=now)
+        return self.filter(state=PublishedStateOptions.PUBLISHED, published_timestamp__lte=now)
 
 
 class VideoManager(models.Manager):
@@ -17,21 +26,15 @@ class VideoManager(models.Manager):
     def published(self):
         return self.get_queryset().published()
 
-class Video(models.Model):
-    class VideoStateOptions(models.TextChoices):
-        # CONSTANT = DB_VALUE, USER_DISPLAY_VA
-        PUBLISHED = 'PU', 'Publish'
-        DRAFT = 'DR', 'Draft'
-        # UNLISTED = 'UN', 'Publish'
-        # PRIVATE = 'PR', 'Private'
 
+class Video(models.Model):
     title = models.CharField(max_length=150)
     description = models.TextField()
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=100, unique=True)
     is_active = models.BooleanField(default=True)
     state = models.CharField(
-        max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+        max_length=2, choices=PublishedStateOptions.choices, default=PublishedStateOptions.DRAFT)
     published_timestamp = models.DateTimeField(
         auto_now_add=False, auto_now=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -46,15 +49,6 @@ class Video(models.Model):
     def is_published(self):
         return self.is_active
 
-    def save(self, *args, **kwargs):
-        if self.state == self.VideoStateOptions.PUBLISHED and self.published_timestamp is None:
-            self.published_timestamp = timezone.now()
-        elif self.state == self.VideoStateOptions.DRAFT:
-            self.published_timestamp = None
-        if self.slug is None:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
 
 class VideoPublishedProxy(Video):
     class Meta:
@@ -68,3 +62,21 @@ class VideoAllProxy(Video):
         proxy = True
         verbose_name = 'All Video'
         verbose_name_plural = 'All Videos'
+
+
+def publish_state_pre_save(sender, instance, *args, **kwargs):
+    is_publish = instance.state == PublishedStateOptions.PUBLISHED
+    is_draft = instance.state == PublishedStateOptions.DRAFT
+    if is_publish and instance.published_timestamp is None:
+        instance.published_timestamp = timezone.now()
+    elif is_draft:
+        instance.published_timestamp = None
+
+
+def slugify_pre_save(sender, instance, *args, **kwargs):
+    if instance.slug is None:
+        instance.slug = slugify(instance.title)
+
+
+pre_save.connect(publish_state_pre_save, sender=Video)
+pre_save.connect(slugify_pre_save, sender=Video)
--- videos/tests.py
+++ videos/tests.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Video
+from .models import Video, PublishedStateOptions
 
 
 class VideoModelTestCase(TestCase):
     def setUp(self):
         self.video1 = Video.objects.create(title='This is my video!', video_id='123')
         self.video2 =Video.objects.create(title='This is life!',
-                             state=Video.VideoStateOptions.PUBLISHED, video_id='456')
+                             state=PublishedStateOptions.PUBLISHED, video_id='456')
     def test_slug_field(self):
         self.assertEqual(self.video1.slug, 'this-is-my-video')
         self.assertEqual(self.video2.slug, 'this-is-life')
@@ -23,13 +23,13 @@ class VideoModelTestCase(TestCase):
         self.assertEqual(qs.count(), 2)
 
     def test_draft_case(self):
-        qs = Video.objects.filter(state=Video.VideoStateOptions.DRAFT)
+        qs = Video.objects.filter(state=PublishedStateOptions.DRAFT)
         self.assertEqual(qs.count(), 1)
 
     def test_published_case(self):
         now = timezone.now()
         published_qs = Video.objects.filter(
-            state=Video.VideoStateOptions.PUBLISHED, created_at__lte=now)
+            state=PublishedStateOptions.PUBLISHED, created_at__lte=now)
         self.assertTrue(published_qs.exists())
 
     def test_publish_manager(self):
