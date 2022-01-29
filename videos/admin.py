12a4f912eba5385396da339e5c0bd281a5d2bf93
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-29 09:26:02 -0300

    add: state field  in video model

--- videos/admin.py
+++ videos/admin.py
@@ -4,8 +4,8 @@ from .models import Video, VideoPublishedProxy, VideoAllProxy
 
 
 class VideoAllAdmin(admin.ModelAdmin):
-    list_display = ['title', 'video_id', 'is_published']
-    search_fields = ['title']
+    list_display = ['title', 'video_id', 'is_published', 'state']
+    search_fields = ['title', 'state']
     list_filter = ['is_active']
     readonly_fields = ['id', 'is_published', 'timestamp']
 
--- videos/models.py
+++ videos/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 
 class Video(models.Model):
+    class VideoStateOptions(models.TextChoices):
+        # CONSTANT = DB_VALUE, USER_DISPLAY_VA
+        PUBLISHED = 'PU', 'Publish'
+        DRAFT = 'DR', 'Draft'
+        # UNLISTED = 'UN', 'Publish'
+        # PRIVATE = 'PR', 'Private'
+
     title = models.CharField(max_length=150)
     description = models.TextField()
     slug = models.SlugField(blank=True, null=True)
     video_id = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
+    state = models.CharField(
+        max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
 
     def __str__(self) -> str:
         return f'id: {self.id} - title: {self.title}'
@@ -25,7 +34,6 @@ class VideoPublishedProxy(Video):
         proxy = True
         verbose_name = 'Published Video'
         verbose_name_plural = 'Published Videos'
-
 
 
 class VideoAllProxy(Video):
