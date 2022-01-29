13d93bff74e9c1f5f1c5a93221abbebd94c6d883
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-29 09:34:26 -0300

    add: created_at field in video model

--- videos/admin.py
+++ videos/admin.py
@@ -6,8 +6,8 @@ from .models import Video, VideoPublishedProxy, VideoAllProxy
 class VideoAllAdmin(admin.ModelAdmin):
     list_display = ['title', 'video_id', 'is_published', 'state']
     search_fields = ['title', 'state']
-    list_filter = ['is_active']
-    readonly_fields = ['id', 'is_published', 'timestamp']
+    list_filter = ['is_active', 'state']
+    readonly_fields = ['id', 'is_published', 'created_at']
 
     class Meta:
         model = VideoAllProxy
--- videos/models.py
+++ videos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 
 class Video(models.Model):
     class VideoStateOptions(models.TextChoices):
@@ -16,17 +16,26 @@ class Video(models.Model):
     is_active = models.BooleanField(default=True)
     state = models.CharField(
         max_length=2, choices=VideoStateOptions.choices, default=VideoStateOptions.DRAFT)
+    created_at = models.DateTimeField(
+        auto_now_add=False, auto_now=False, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'id: {self.id} - title: {self.title}'
 
     @property
-    def timestamp(self):
-        return None
-
-    @property
     def is_published(self):
         return self.is_active
+    
+    def save(self, *args, **kwargs):
+        if self.state == self.VideoStateOptions.PUBLISHED and self.created_at is None:
+            print('save as timestamp for published')
+            self.created_at = timezone.now()
+        elif self.state == self.VideoStateOptions.DRAFT:
+            self.created_at = None
+        super().save(*args, **kwargs)
+        
+
+    
 
 
 class VideoPublishedProxy(Video):
