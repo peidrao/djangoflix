235a0ae81a67cb8840c87463cba8c3069a0a2e93
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-30 18:14:30 -0300

    add: category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=220)
+    slug = models.SlugField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
