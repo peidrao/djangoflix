cb08080800dae55ac52c0ffa7d667732e373c3fd
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-30 18:17:15 -0300

    add: category test

--- categories/tests.py
+++ categories/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Category
+
+
+class CategoryTestCase(TestCase):
+
+    def setUp(self):
+        self.cat_a = Category.objects.create(title='Action')
+        self.cat_b = Category.objects.create(title='Comedy', is_active=False)
+
+    def test_is_active(self):
+        self.assertTrue(self.cat_a.is_active)
+
+    def test_not_is_active(self):
+        self.assertFalse(self.cat_b.is_active)
