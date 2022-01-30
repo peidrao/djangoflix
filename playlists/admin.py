3195027096b74fd1d63cda4b38251c1c3b3473ca
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-30 10:23:28 -0300

    update: custom playlist admin

--- playlists/admin.py
+++ playlists/admin.py
@@ -1,6 +1,39 @@
 from django.contrib import admin
 
-from .models import Playlist, PlaylistItem
+from .models import Playlist, PlaylistItem, TVShowProxy, TVShowSeasonProxy
+
+
+class SeasonEpisodeInline(admin.TabularInline):
+    model = PlaylistItem
+    extra = 0
+
+
+class TVShowSeasonProxyAdmin(admin.ModelAdmin):
+    inlines = [SeasonEpisodeInline]
+    list_display = ['title', 'parent']
+
+    class Meta:
+        model = TVShowSeasonProxy
+    
+    def get_queryset(self, request):
+        return TVShowSeasonProxy.objects.all()
+
+
+class TVShowSeasonProxyInline(admin.TabularInline):
+    model = TVShowSeasonProxy
+    extra = 2
+    fields = ['order', 'title', 'state']
+
+
+class TVShowProxyAdmin(admin.ModelAdmin):
+    inlines = [TVShowSeasonProxyInline]
+    fields = ['title', 'description', 'state', 'video', 'slug']
+
+    class Meta:
+        model = TVShowProxy
+
+    def get_queryset(self, request):
+        return TVShowProxy.objects.all()
 
 
 class PlaylistItemInline(admin.TabularInline):
@@ -21,3 +54,5 @@ class PlaylistAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Playlist, PlaylistAdmin)
+admin.site.register(TVShowProxy, TVShowProxyAdmin)
+admin.site.register(TVShowSeasonProxy, TVShowSeasonProxyAdmin)
