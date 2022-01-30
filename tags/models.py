3818f6a8a4f285ecd45208d67770d8227c7326a2
Pedro Fonseca <contatopedrorn@gmail.com>
2022-01-30 18:40:27 -0300

    add: tag model and admin

--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+from django_flix.db.receivers import slugify_pre_save
+
+
+class TaggedItem(models.Model):
+    tag = models.SlugField(blank=True, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.tag)
+
+
+pre_save.connect(slugify_pre_save, sender=TaggedItem)
