27fc8848275f5c96fdaa6d4cf0ee84c17d318e81
Pedro Fonseca <contatopedrorn@gmail.com>
2022-02-12 18:47:31 -0300

    add: detail views

--- django_flix/urls.py
+++ django_flix/urls.py
@@ -2,18 +2,30 @@
 from django.contrib import admin
 from django.urls import path, re_path
 
-from playlists.views import FeaturedPlaylistListView, MovieListView, TVShowListView
+from playlists.views import (
+    FeaturedPlaylistListView, 
+    MovieListView, 
+    PlaylistDetailView,
+    TVShowDetailView,
+    TVShowListView,
+    MovieDetailView,
+    TVShowSeasonDetailView
+)
+
+
 
 urlpatterns = [
     re_path(r'my-detail/(?P<id>\d+)/$', FeaturedPlaylistListView.as_view()),
     path('', FeaturedPlaylistListView.as_view()),
     path('admin/', admin.site.urls),
     path('movies/', MovieListView.as_view()),
-    path('movies/<slug:slug>', MovieListView.as_view()),
+    path('movies/<slug:slug>', MovieDetailView.as_view()),
+    path('playlist/<slug:slug>', PlaylistDetailView.as_view()),
     path('media/<int:id>', FeaturedPlaylistListView.as_view()),
-    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>', TVShowListView.as_view()),
-    path('shows/<slug:slug>/seasons/', TVShowListView.as_view()),
-    path('shows/<slug:slug>/', TVShowListView.as_view()),
+    path('shows/<slug:showSlug>/seasons/<slug:seasonSlug>',
+         TVShowSeasonDetailView.as_view()),
+    path('shows/<slug:slug>/seasons/', TVShowSeasonDetailView.as_view()),
+    path('shows/<slug:slug>/', TVShowDetailView.as_view()),
     path('shows/', TVShowListView.as_view()),
 
 ]
--- playlists/views.py
+++ playlists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from .models import MovieProxy, Playlist, TVShowProxy
+from .models import MovieProxy, Playlist, TVShowProxy, TVShowSeasonProxy
 # Create your views here.
 
 
@@ -13,13 +13,45 @@ class TitleMixin():
         if self.title is not None:
             context['title'] = self.title
         return context
-    
 
 
 class MovieListView(TitleMixin, generic.ListView):
     template_name = 'playlist_list.html'
     queryset = MovieProxy.objects.all()
     title = 'Movies'
+
+
+class PlaylistDetailView(TitleMixin, generic.DetailView):
+    template_name = 'playlist/playlist_detail.html'
+    queryset = Playlist.objects.all()
+    title = 'Playlist'
+
+
+class MovieDetailView(TitleMixin, generic.DetailView):
+    template_name = 'playlist/movie_detail.html'
+    queryset = MovieProxy.objects.all()
+    title = 'Movies'
+
+
+class TVShowDetailView(TitleMixin, generic.DetailView):
+    template_name = 'playlist/tvshow_detail.html'
+    queryset = TVShowProxy.objects.all()
+    title = 'Movies'
+
+
+class TVShowSeasonDetailView(TitleMixin, generic.DetailView):
+    template_name = 'playlist/tvshow_seasons_detail.html'
+    queryset = TVShowSeasonProxy.objects.all()
+
+    def get_object(self):
+        show_slug = self.kwargs.get('showSlug')
+        season_slug = self.kwargs.get('seasonSlug')
+        qs = self.get_queryset().filter(
+            parent__slug__iexact=show_slug, slug__iexact=season_slug)
+        
+        if not qs.count() == 1:
+            raise Exception('Not found')
+        return qs.first()
 
 
 class TVShowListView(TitleMixin, generic.ListView):
